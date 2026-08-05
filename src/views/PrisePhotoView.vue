73d9ba7109ad036ffaa6f205040bccb4fd41d499
Gabriel Poma <gpoma@24eme.fr>
2026-08-05 11:43:51 +0200

    prettier

--- src/views/PrisePhotoView.vue
+++ src/views/PrisePhotoView.vue
@@ -1,34 +1,39 @@
 <script setup>
-  import { ref } from 'vue'
-  import { pointData } from '../store/pointData.js'
+import { ref } from "vue";
+import { pointData } from "@/store/pointData.js";
 
-  const inputPhoto = ref(null)
+const inputPhoto = ref(null);
 
-  function ouvrirCamera() {
-    inputPhoto.value.click()
-  }
+function ouvrirCamera() {
+    inputPhoto.value.click();
+}
 
-  function onPhotoSelected(event) {
-    const file = event.target.files[0]
+function onPhotoSelected(event) {
+    const file = event.target.files[0];
     if (file) {
-      pointData.photo = URL.createObjectURL(file)
+        pointData.photo = URL.createObjectURL(file);
     }
-  }
+}
 </script>
 
 <template>
-<div class="text-center">
-    <h1 class="mb-5">Ajout du piège</h1>
-    <input
-        ref="inputPhoto"
-        type="file"
-        accept="image/*"
-        capture="environment"
-        style="display: none"
-        @change="onPhotoSelected"
-    />
+    <div class="text-center">
+        <h1 class="mb-5">Ajout du piège</h1>
+        <input
+            ref="inputPhoto"
+            type="file"
+            accept="image/*"
+            capture="environment"
+            style="display: none"
+            @change="onPhotoSelected"
+        />
 
-    <img v-if="pointData.photo" :src="pointData.photo" class="mt-3" style="max-width: 100%;" />
+        <img
+            v-if="pointData.photo"
+            :src="pointData.photo"
+            class="mt-3"
+            style="max-width: 100%"
+        />
 
         <div class="d-grid gap-2 col-11 mx-auto mt-2">
             <button
@@ -55,8 +60,6 @@
             </RouterLink>
         </div>
     </div>
-</div>
 </template>
 
-<style>
-</style>
+<style></style>
