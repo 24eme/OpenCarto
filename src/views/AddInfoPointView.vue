ce61c077df10c6b07f495c00f9fa44d3ebfc1d1a
Gabriel Poma <gpoma@24eme.fr>
2026-08-07 20:10:27 +0200

    css info point

--- src/views/AddInfoPointView.vue
+++ src/views/AddInfoPointView.vue
@@ -8,47 +8,85 @@ if (!pointData.idPiege || pointData.idPiege === "0") {
 </script>
 
 <template>
-    <div class="text-center">
-        <h1 class="mb-5">Ajout d'un point</h1>
+    <div class="vstack h-100">
+        <nav class="navbar bg-body-tertiary">
+            <div class="container justify-content-start">
+                <div class="col-auto d-flex me-3">
+                    <RouterLink :to="{ name: 'home' }">
+                        <i
+                            class="bi bi-arrow-left-square fs-1 align-self-center"
+                        ></i
+                    ></RouterLink>
+                </div>
+                <div class="col-auto flex-grow-1">
+                    <h1 class="m-0">Ajouter un point</h1>
+                </div>
+            </div>
+        </nav>
 
-        <h2>Étage</h2>
+        <div class="container vstack justify-content-center">
+            <div class="my-2">
+                <label for="etageSelect" class="h4 form-label"
+                    >Choix de l'étage</label
+                >
+                <select
+                    id="etageSelect"
+                    class="form-select"
+                    v-model="pointData.etage"
+                >
+                    <option v-for="etage in etagesConfig" :value="etage.id">
+                        {{ etage.label }}
+                    </option>
+                </select>
+            </div>
 
+            <div class="my-2">
+                <label for="secteurText" class="h4 form-label">Secteur</label>
+
+                <input
+                    id="secteurText"
+                    class="form-control"
+                    type="text"
+                    placeholder="ex. : Faux plafond, ..."
+                    v-model="pointData.secteur"
+                />
+            </div>
+
+            <div class="my-2">
+                <label for="typeSelect" class="h4 form-label"
+                    >Type de piège</label
+                >
+                <select
+                    id="typeSelect"
+                    class="form-select"
+                    aria-label="select-type-piege"
+                    v-model="pointData.typePiege"
+                >
+                    <option value="Câlin">Câlin</option>
+                    <option value="Bisous">Bisous</option>
+                    <option value="Carresse">Carresse</option>
+                </select>
+            </div>
+
+            <div class="my-2">
+                <label for="idPiegeInput" class="h4 form-label"
+                    >Numéro du piège</label
+                >
+                <br />
+                <small>à reporter sur l'installation</small>
+
+                <!-- ; -->
+                <input
+                    type="text"
+                    class="form-control disabled"
+                    disabled
+                    v-model="pointData.idPiege"
+                />
+            </div>
+        </div>
         <!-- mettre par défaut l'étage sur lequel il est -->
-        <select v-model="pointData.etage">
-            <option v-for="etage in etagesConfig" :value="etage.id">
-                {{ etage.label }}
-            </option>
-        </select>
 
-        <h2 class="mt-5">Secteur</h2>
-
-        <input
-            type="text"
-            placeholder="ex. : faux plafond, ..."
-            v-model="pointData.secteur"
-        />
-
-        <h2 class="mt-5">Type de piège</h2>
-        <select
-            class="form-select"
-            aria-label="select-type-piege"
-            v-model="pointData.typePiege"
-        >
-            <option value="Câlin">Câlin</option>
-            <option value="Bisous">Bisous</option>
-            <option value="Carresse">Carresse</option>
-        </select>
-
-        <h2 class="mt-5 mb-0">Numéro du piège</h2>
-        <small>à reporter sur l'installation</small>
-        <br />
-
-        <!-- ; -->
-        <span class="border">
-            <span class="px-3">{{ pointData.idPiege }}</span>
-        </span>
-
-        <div class="d-grid gap-2 col-11 mx-auto mb-2 fixed-bottom">
+        <div class="m-1 row">
             <RouterLink :to="{ name: 'prisePhoto' }" class="btn btn-primary">
                 Valider les informations
             </RouterLink>
