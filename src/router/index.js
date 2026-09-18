b48bb5fb45c1f9674353a8e51867f6d705d3f437
Gabriel Poma <gpoma@24eme.fr>
2026-09-18 15:39:24 +0200

    Page d'accueil listing étage et nb pièges

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from "@/views/HomeView.vue";
 import EtageView from "@/views/EtageView.vue";
 import AddInfoPointView from '../views/AddInfoPointView.vue'
 import PrisePhotoView from '../views/PrisePhotoView.vue'
@@ -8,7 +9,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/:etage",
       name: "etage",
       component: EtageView,
     },
---
+++ src/views/HomeView.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { etagesConfig } from "@/store/etages";
+import { fetchPoints } from "@/store/points";
+import { ref } from "vue";
+
+const clientName = ref("Potel & Chabot");
+const etages = ref(etagesConfig);
+const points = fetchPoints();
+
+function getNbPoints(etage) {
+    return points.filter((p) => p.etage === etage).length;
+}
+// Possible de le faire en computed, mais complexifie le code
+// const getNbPoints = (etage) =>
+//    computed(() => points.filter((p) => p.etage === etage).length);
+</script>
+
+<template>
+    <main class="container-fluid g-0 vstack fullscreen">
+        <nav class="navbar bg-body-tertiary">
+            <div class="container justify-content-start">
+                <div class="col-auto d-flex me-3">
+                    <i
+                        class="bi bi-arrow-left-square fs-1 align-self-center"
+                    ></i>
+                </div>
+                <div class="col-auto">
+                    <h1 class="m-0">Suivi de l'infestation</h1>
+                    <small>{{ clientName }}</small>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container">
+            <h1 class="mt-4">Liste des étages</h1>
+            <div class="table-responsive fs-5">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Étage</th>
+                            <th class="text-end">Nb de pièges</th>
+                            <th class="ms-auto"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="etage in etages">
+                            <td>{{ etage.label }}</td>
+                            <td class="text-end">
+                                {{ getNbPoints(etage.id) }} points
+                            </td>
+                            <td class="text-end ms-auto">
+                                <RouterLink
+                                    :to="{
+                                        name: 'etage',
+                                        params: { etage: etage.id },
+                                    }"
+                                    >» Voir</RouterLink
+                                >
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </main>
+</template>
+
+<style></style>
