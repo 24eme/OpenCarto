e19a6bd8a9f89aaa7f3b2b10414c6269483079e4
Gabriel Poma <gpoma@24eme.fr>
2026-09-24 11:51:07 +0200

    fix coordonnées header en mode flex

--- src/views/AddCoordonneesPointView.vue
+++ src/views/AddCoordonneesPointView.vue
@@ -30,20 +30,26 @@ function updatePointCoordinate(coordinates) {
 </script>
 
 <template>
-  <nav class="navbar bg-body-tertiary">
-      <div class="container justify-content-start">
-          <div class="col-auto d-flex me-3">
-              <RouterLink :to="{ name: 'prisePhoto' }">
-                  <i class="bi bi-arrow-left-square fs-1 align-self-center" style="color: black;"></i></RouterLink>
-          </div>
-          <div class="col-auto flex-grow-1">
-              <h1 class="m-0">Positionner le point</h1>
-          </div>
-          <p class="text-muted align-center">
-              Fais défiler la carte pour placer le viseur au bon endroit
-          </p>
-      </div>
-  </nav>
+    <main class="container-fluid g-0 vstack fullscreen">
+        <nav class="navbar bg-body-tertiary">
+            <div class="container justify-content-start flex-nowrap">
+                <div class="col-auto d-flex me-3">
+                    <RouterLink :to="{ name: 'prisePhoto' }">
+                        <i
+                            class="bi bi-arrow-left-square fs-1 align-self-center"
+                        ></i
+                    ></RouterLink>
+                </div>
+                <div class="col-auto flex-grow-1 flex-fill">
+                    <h1 class="m-0">Positionner le point</h1>
+                    <small>
+                        Fais défiler la carte pour placer le viseur au bon
+                        endroit
+                    </small>
+                </div>
+            </div>
+        </nav>
+    </main>
     <div class="text-center">
         <div class="carte-wrapper">
             <Carte
